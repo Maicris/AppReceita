e377d5613c35943b0f9cd9041ff6a40f95f278ed
Maicris Fernandes <maicris@yahooo.com.br>
2019-04-23 09:12:16 -0300

    Initial Commit

---
+++ ReceitaApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReceitaApp
+//
+//  Created by Maicris Fernandes on 23/04/19.
+//  Copyright © 2019 Maicris Fernandes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
