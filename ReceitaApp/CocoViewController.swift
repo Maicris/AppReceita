b8b277bd1d40be93d7645242d23d400e72e41f50
Maicris Fernandes <maicris@yahooo.com.br>
2019-04-24 08:41:21 -0300

    Primeiro commit

---
+++ ReceitaApp/CocoViewController.swift
@@ -0,0 +1,34 @@
+//
+//  CocoViewController.swift
+//  ReceitaApp
+//
+//  Created by Maicris Fernandes on 23/04/19.
+//  Copyright © 2019 Maicris Fernandes. All rights reserved.
+//
+
+import UIKit
+
+class CocoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func back(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
